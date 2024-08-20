21adf641370565269a0c0a01b41e35480e99724a
Karan Hathwar <karanhathwar@gmail.com>
2024-08-20 12:51:36 +0530

    Started integraton of Vercel Database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: env.POSTGRES_URL,
   },
   tablesFilter: ["e-commerce-website_*"],
 } satisfies Config;
